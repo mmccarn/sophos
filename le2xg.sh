13121e8673b2578d390ffff253457f8d384c8445
mmccarn <mmccarn-github@mmsionline.us>
2020-02-17 09:30:47 -0500

    Use privkey.pem directly
    Avoid copying privkey.pem out of the letsencrypt folder

--- le2xg.sh
+++ le2xg.sh
@@ -33,8 +33,6 @@ OPERATION=${1:-update}
 #    - listing the 3 files to be uploaded in the order they occur in the input
 # 4. Delete the copy of privkey.pem that was created
 
-cp "/etc/letsencrypt/live/$LEDOMAIN/privkey.pem" ./privkey.key
-
 sed \
                 -e "s/APIUSER/$APIUSER/" \
                 -e "s/APIPLAINPASS/$APIPLAINPASS/" \
@@ -43,7 +41,5 @@ sed \
 | curl -k -F "reqxml=<-" \
   -F file=@/etc/letsencrypt/live/$LEDOMAIN/chain.pem \
   -F file=@/etc/letsencrypt/live/$LEDOMAIN/fullchain.pem \
-  -F file=@./privkey.key \
+  -F file=@/etc/letsencrypt/live/$LEDOMAIN/privkey.pem \
   "https://$ROUTER/webconsole/APIController?"
-
-rm ./privkey.key
